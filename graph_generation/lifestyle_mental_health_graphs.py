b500b46d5d71646f14bf33cd62fdb070ea99f264
Nguyen-Kha <khnguyen26@gmail.com>
2024-03-04 00:42:56 -0600

    add  disciplines of interest bar graphs

--- graph_generation/lifestyle_mental_health_graphs.py
+++ graph_generation/lifestyle_mental_health_graphs.py
@@ -11,3 +11,23 @@ import functools
 
 df = pd.read_csv('')
 df1 = pd.read_csv('')
+
+df_pa = df_pa = pd.read_csv('professional_activities.csv')
+
+#### Create disciplines of interest bar #####################
+df_disciplines = df_pa[['disciplines_of_interest']]
+df_disciplines = df_disciplines.dropna()
+
+graphs.create_bar(
+    df_disciplines,
+    'disciplines_of_interest',
+    'Disciplines',
+    'Number of respondents',
+    'What are your disciplines of interest',
+    vertical = False,
+    splice_required = True,
+    figure_height = 12,
+    max_label_length = 30,
+)
+#### END: Create disciplines of interest bar ################
+
