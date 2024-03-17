aaf8c76bb4fb61c33df3b9907796368e8ea48f91
Nguyen-Kha <khnguyen26@gmail.com>
2024-03-17 16:10:10 -0500

    add friends in undergrad bar

--- graph_generation/lifestyle_mental_health_graphs.py
+++ graph_generation/lifestyle_mental_health_graphs.py
@@ -5,7 +5,7 @@ import math
 import matplotlib.pyplot as plt   # use to make graphs
 import seaborn as sns
 from collections import Counter
-# import helpers
+import helpers
 import graphs
 import functools
 
@@ -13,6 +13,7 @@ df = pd.read_csv('')
 df1 = pd.read_csv('')
 
 df_pa = df_pa = pd.read_csv('professional_activities.csv')
+df_social = pd.read_csv('social.csv')
 
 #### Create disciplines of interest bar #####################
 df_disciplines = df_pa[['disciplines_of_interest']]
@@ -58,3 +59,23 @@ graphs.create_bar(
     graph_name_labels = ['Yes, Cali-ed', 'Yes, Busted', 'No']
 )
 #### END: Create cali or bust bar ###############
+
+#### create friends in undergrad bar ##############
+df_friends_ug = df_social[['friends_undergrad']].copy()
+df_friends_ug['friends_undergrad'] = df_friends_ug['friends_undergrad'].apply(helpers.turn_dates_into_actual_values)
+df_friends_ug = df_friends_ug.dropna()
+
+friends_undergrad_list = ['1 - 5', '6 - 10', '11 - 15', '16 - 20', '21 - 25', '26 - 30', '30+']
+
+graphs.create_bar(
+    df_friends_ug,
+    'friends_undergrad',
+    'Number of friends in undergrad',
+    'Percentage of Respondents',
+    'How many friends did you have in undergrad?',
+    vertical = True,
+    display_as_percentage = True,
+    labels = friends_undergrad_list,
+    values_increment = 5
+)
+#### END: create friends in undergrad bar #############
